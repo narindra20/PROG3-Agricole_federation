55d9a03f48445c01fa2a7ef76fbc123f528b876b
narindra20 <hei.narindra@gmail.com>
2026-04-21 15:54:32 +0300

    Connection database

--- src/main/java/hei/school/agricole/config/DataSource.java
+++ src/main/java/hei/school/agricole/config/DataSource.java
@@ -16,7 +16,7 @@ public class DataSource {
     @Bean
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/federation_agricole", "postgres", "postgres");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/agricultural_federation", "postgres", "postgres");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
