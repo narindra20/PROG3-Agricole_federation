30d82ec436221689c7ccb21a0bf49a9bc8ea68d6
narindra20 <hei.narindra@gmail.com>
2026-05-07 10:17:16 +0300

    Update Collectivites to Collectivities

--- src/main/java/hei/school/agricole/controller/ActivityController.java
+++ src/main/java/hei/school/agricole/controller/ActivityController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/collectivites")
+@RequestMapping("/collectivities")
 public class ActivityController {
 
     private final ActivityService activityService;
